35640dd2d5c1edd621be7e16628109bda24209c4
MEGATr0nnn <harrison.mega@outlook.com>
2024-09-03 10:29:03 +1000

    fixed broken stuff

--- Project Bundle/Real Estate Managent Software/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.real_estate_managent_software {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.real_estate_managent_software to javafx.fxml;
-    exports com.example.real_estate_managent_software;
-}
